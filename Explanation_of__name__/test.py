61dc29ebff5d6fad01b68919d7794654a90f8920
charliescode <charles.gold@turntabl.io>
2022-01-24 12:12:33 +0000

    how to write unit test in python

---
+++ Explanation_of__name__/test.py
@@ -0,0 +1,7 @@
+import Explanation_of__name__.file1 as math_pk
+import Explanation_of__name__.file2 as shop_pk
+
+print(math_pk.multiply(2, 4))
+print(shop_pk.shop_item("banana"))
+
+print(__name__)
